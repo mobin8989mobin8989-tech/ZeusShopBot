e456389c05c71e99295144fd36a70181ae9e65e3
mobin8989mobin8989-tech <mobin8989mobin8989@gmail.com>
2026-07-28 13:09:47 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,72 @@
+# ==========================================================
+# ZeusShopBot
+# bot.py
+# ==========================================================
 
+from telegram import Update
+from telegram.ext import (
+    Application,
+    CommandHandler,
+    CallbackQueryHandler,
+    MessageHandler,
+    ContextTypes,
+    filters,
+)
+
+from config import BOT_TOKEN
+
+
+class ZeusBot:
+
+    def __init__(self):
+
+        self.app = Application.builder().token(BOT_TOKEN).build()
+
+    # ======================================================
+
+    async def start(
+        self,
+        update: Update,
+        context: ContextTypes.DEFAULT_TYPE,
+    ):
+
+        await update.message.reply_text(
+            "👋 به Zeus Shop خوش آمدید."
+        )
+
+    # ======================================================
+
+    async def help(
+        self,
+        update: Update,
+        context: ContextTypes.DEFAULT_TYPE,
+    ):
+
+        await update.message.reply_text(
+            "پنل مدیریت در حال آماده سازی است."
+        )
+
+    # ======================================================
+
+    def register_handlers(self):
+
+        self.app.add_handler(
+            CommandHandler("start", self.start)
+        )
+
+        self.app.add_handler(
+            CommandHandler("help", self.help)
+        )
+
+    # ======================================================
+
+    def run(self):
+
+        self.register_handlers()
+
+        print("Zeus Shop Started...")
+
+        self.app.run_polling()
+
+
+bot = ZeusBot()
