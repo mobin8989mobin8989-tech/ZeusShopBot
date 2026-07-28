8c366ecb5c0d7be03dfdf22bb08913dffe298d3f
mobin8989mobin8989-tech <mobin8989mobin8989@gmail.com>
2026-07-28 23:49:20 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,23 +3,14 @@
 # bot.py
 # ==========================================================
 
-
-from telegram import Update
-
-from telegram.ext import (
-    Application,
-    CommandHandler,
-    ContextTypes,
-)
+from telegram.ext import Application
 
 from config import BOT_TOKEN
 
 from handlers import register_handlers
 
 
-
 class ZeusBot:
-
 
     def __init__(self):
 
@@ -32,74 +23,37 @@ class ZeusBot:
 
 
     # ======================================================
-    # Start
-    # ======================================================
-
-    async def start(
-        self,
-        update: Update,
-        context: ContextTypes.DEFAULT_TYPE
-    ):
-
-        await update.message.reply_text(
-            "👑 به Zeus Shop VPN خوش آمدید."
-        )
-
-
-
-    # ======================================================
     # Register
     # ======================================================
 
     def register_handlers(self):
-
-
-        # اتصال تمام بخش‌های handlers.py
 
         register_handlers(
             self.app
         )
 
 
-        # دستور start
-
-        self.app.add_handler(
-            CommandHandler(
-                "start",
-                self.start
-            )
-        )
-
-
-
     # ======================================================
-    # Run
+    # Run Bot
     # ======================================================
 
     def run(self):
 
-
         self.register_handlers()
-
 
         print(
             "🚀 Zeus Shop Bot Started..."
         )
 
-
         self.app.run_polling()
 
 
 
-
-
 # ==========================================================
-# Run Bot
+# Start
 # ==========================================================
-
 
 if __name__ == "__main__":
-
 
     bot = ZeusBot()
 
