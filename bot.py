d307662379e89007130967540c0fa761ec5b5c02
mobin8989mobin8989-tech <mobin8989mobin8989@gmail.com>
2026-07-28 23:46:26 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,70 +3,104 @@
 # bot.py
 # ==========================================================
 
+
 from telegram import Update
+
 from telegram.ext import (
     Application,
     CommandHandler,
-    CallbackQueryHandler,
-    MessageHandler,
     ContextTypes,
-    filters,
 )
 
 from config import BOT_TOKEN
 
+from handlers import register_handlers
+
+
 
 class ZeusBot:
 
+
     def __init__(self):
 
-        self.app = Application.builder().token(BOT_TOKEN).build()
+        self.app = (
+            Application
+            .builder()
+            .token(BOT_TOKEN)
+            .build()
+        )
 
+
+    # ======================================================
+    # Start
     # ======================================================
 
     async def start(
         self,
         update: Update,
-        context: ContextTypes.DEFAULT_TYPE,
+        context: ContextTypes.DEFAULT_TYPE
     ):
 
         await update.message.reply_text(
-            "👋 به Zeus Shop خوش آمدید."
+            "👑 به Zeus Shop VPN خوش آمدید."
         )
+
+
 
     # ======================================================
-
-    async def help(
-        self,
-        update: Update,
-        context: ContextTypes.DEFAULT_TYPE,
-    ):
-
-        await update.message.reply_text(
-            "پنل مدیریت در حال آماده سازی است."
-        )
-
+    # Register
     # ======================================================
 
     def register_handlers(self):
 
-        self.app.add_handler(
-            CommandHandler("start", self.start)
+
+        # اتصال تمام بخش‌های handlers.py
+
+        register_handlers(
+            self.app
         )
 
+
+        # دستور start
+
         self.app.add_handler(
-            CommandHandler("help", self.help)
+            CommandHandler(
+                "start",
+                self.start
+            )
         )
 
+
+
+    # ======================================================
+    # Run
     # ======================================================
 
     def run(self):
 
+
         self.register_handlers()
 
-        print("Zeus Shop Started...")
+
+        print(
+            "🚀 Zeus Shop Bot Started..."
+        )
+
 
         self.app.run_polling()
 
 
-bot = ZeusBot()
+
+
+
+# ==========================================================
+# Run Bot
+# ==========================================================
+
+
+if __name__ == "__main__":
+
+
+    bot = ZeusBot()
+
+    bot.run()
